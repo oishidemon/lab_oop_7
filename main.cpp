5d2fd9339b8daaaa3fde4d5399b06d5458833426
oishidemon <orobets.oleh@chnu.edu.ua>
2024-05-10 12:09:17 +0300

    Checked functionality for lists.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,11 @@ int main() {
     list.push(2);
     list.push(3);
     list.push(4);
-    list.push(5);
     list.atInsert(0,0);
     list.show();
     list.atRemove(1);
     list.show();
-    cout<<"Found: "<<list.search(1)<<endl;
+    cout<<"find: "<<list.search(1)<<endl;
 
     return 0;
 }
