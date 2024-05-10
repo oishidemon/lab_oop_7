4473d22d17c16b058b86e290ad13fdb38345af50
oishidemon <orobets.oleh@chnu.edu.ua>
2024-05-10 11:53:01 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "SingleLinkedList.h"
+#include "DoubleLinkedList.h"
+using namespace std;
+int main() {
+    SingleLinkedList<int> list;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.atInsert(0,0);
+    list.show();
+    list.atRemove(1);
+    list.show();
+    cout<<"Found: "<<list.search(1)<<endl;
+
+    return 0;
+}
+// aleg imba
